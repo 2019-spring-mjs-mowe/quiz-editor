12032db05a894ced2ccc6e43dc6aa9d7f4f424f8
Michael Hall <47337730+mHall31@users.noreply.github.com>
2019-04-08 15:11:34 -0500

    imported modules and added REST call
    OnInit and HttpClient(module) imported to relevent locations
    
    removed hardcoded quiz list, calling out to service to load list

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { }
 
   getQuizzes() {
+    return this.builtInAngularHttpClient
+    .get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz");
+   
     //Mocking the data we'd recieve from a REST endpoint or service call
-    return [
-      { name: 'Quiz 1', numberOfQuestions: 3}
-      , { name: 'Quiz 2', numberOfQuestions: 0}
-      , { name: 'Quiz 3', numberOfQuestions: 0}
+    // return [
+    //   { name: 'Quiz 1', numberOfQuestions: 3}
+    //   , { name: 'Quiz 2', numberOfQuestions: 0}
+    //   , { name: 'Quiz 3', numberOfQuestions: 0}
 
-    ]
+    // ]
+
   }
 }
