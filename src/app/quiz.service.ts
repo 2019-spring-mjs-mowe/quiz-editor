bbcd4612c0725de75a1dd7e4f93f3ead7f63c8cb
Luke Busch <lmbusch@madisoncollege.edu>
2019-04-08 15:14:28 -0500

    quiz service now calls REST endpoint
    added a bunch of dependencies to use an endpoint, implemented ngOnInit()

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private builtInAngularHttpClient: HttpClient) { 
+    
+   }
 
   getQuizzes() {
 
-    // Mocking up the data we will ultimately get from a 
-    // REST endpoint, or web service call...
-    return [
-      { name: 'Quiz 1', numberQuestions: 3 }
-      , { name: 'Quiz 2', numberQuestions: 0 }
-      , { name: 'Quiz 3', numberQuestions: 0 }
-    ].map(x => ({
-      name: x.name
-      , numberOfQuestions: x.numberQuestions
-    }));
+    return this.builtInAngularHttpClient.get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz");
+    
   }
 }
