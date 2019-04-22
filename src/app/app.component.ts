a7447e4d04f892c974f660083ca267b36f45df66
Aseng Vue <35584537+asevue@users.noreply.github.com>
2019-04-22 15:23:43 -0500

    commiting changes from animation implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from './quiz.service';
+import {
+  trigger
+  , transition
+  , animate
+  , keyframes
+  , style
+} from '@angular/animations';
+
 
 interface QuizDisplay {
   name: string;
@@ -16,7 +24,29 @@ interface QuestionDisplay {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  animations: [
+    trigger('detailsFromLeft', [
+      transition('leftPosition => finalPosition', [
+        animate('300ms', keyframes([
+          style({ left: '-30px', offset: 0.0 }),
+          style({ left: '-20px', offset: 0.25 }),
+          style({ left: '-10px', offset: 0.5 }),
+          style({ left: '-5px', offset: 0.75 }),
+          style({ left: '0px', offset: 1.0 })
+        ]))
+      ]),
+    ]),
+    trigger('pulseSaveCancelButtons', [
+      transition('nothingToSave => somethingToSave', [
+        animate('400ms', keyframes([
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 0.0 }),
+          style({ transform: 'scale(1.2)', 'transform-origin': 'top left', offset: 0.5 }),
+          style({ transform: 'scale(1.0)', 'transform-origin': 'top left', offset: 1.0 })
+        ]))
+      ])
+    ])
+  ]
 })
 export class AppComponent implements OnInit {
 
@@ -53,6 +83,7 @@ export class AppComponent implements OnInit {
 
   setSelectedQuiz(q: QuizDisplay) {
     this.selectedQuiz = q;
+    this.detailsAnimationState = 'finalPosition';
   }
 
   get titleColor() {
@@ -76,7 +107,7 @@ export class AppComponent implements OnInit {
       , markedForDelete: true
     }
     this.quizzes = [...this.quizzes, newQuiz];
-    this.selectedQuiz = newQuiz;
+    this.setSelectedQuiz(newQuiz);
   }
 
   questions: QuestionDisplay[] = [];
@@ -161,6 +192,12 @@ export class AppComponent implements OnInit {
 
   get numberOfAddedQuizzes() {
     return this.quizzes.filter(x => x.originalName === 'New Untitled Quiz').length;
+  }
+
+  detailsAnimationState = "leftPostition";
+
+  detailsFromLeftAnimationComplete() {
+    this.detailsAnimationState = 'leftPostition';
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
