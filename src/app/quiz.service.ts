8850b3ee3663f2731a592ecee6c9ba7e32dfc2c4
shong3 <42481531+shong3@users.noreply.github.com>
2019-04-24 15:25:44 -0500

    Finishing with Save

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +8,29 @@ export class QuizService {
 
   constructor(private builtInAngularHttpClient: HttpClient) { }
 
+  saveQuizzes(changedQuizzes: any[], newQuizzes: any[] = []) {
+
+    let h = new HttpHeaders({
+      'Content-Type': 'application/json'
+      , 'X-Sas-Token': 'sig=K2WE6NQPtyoV6ke5hwPEaEaW52fgvyFWUeCEdPJls1s'
+    });
+
+    //console.log(h);
+
+    return this.builtInAngularHttpClient.post(
+      'https://modern-js.azurewebsites.net/save-quizzes-proxy'
+      , JSON.stringify(
+        {
+          "changedQuizzes": changedQuizzes
+          , "newQuizzes": newQuizzes
+        }
+      )
+      , {
+        headers: h
+      }
+    );
+  }
+  
   getQuizzes() {
 
     return this.builtInAngularHttpClient.get("https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz");
